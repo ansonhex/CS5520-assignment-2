6c4308c841360d3c44ca6f5ce46901869b5f6511
ansonhex <ansonhe1997@gmail.com>
2024-10-02 12:09:49 -0700

    added vector icons

--- App.js
+++ App.js
@@ -7,7 +7,9 @@ import Activities from "./Screens/Activities";
 import AddActivities from "./Screens/AddActivities";
 import Diet from "./Screens/Diet";
 import AddDiet from "./Screens/AddDiet";
-
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -33,7 +35,24 @@ function DietStack() {
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            if (route.name === "ActivitiesTab") {
+              return <FontAwesome5 name="running" size={size} color={color} />;
+            } else if (route.name === "DietTab") {
+              return (
+                <MaterialIcons name="fastfood" size={size} color={color} />
+              );
+            } else if (route.name === "Settings") {
+              return <Ionicons name="settings" size={size} color={color} />;
+            }
+          },
+          tabBarActiveTintColor: "orange",
+          tabBarInactiveTintColor: "gray",
+          tabBarStyle: [{ display: "flex" }],
+        })}
+      >
         <Tab.Screen name="ActivitiesTab" component={ActivitiesStack} />
         <Tab.Screen name="DietTab" component={DietStack} />
         <Tab.Screen name="Settings" component={Settings} />

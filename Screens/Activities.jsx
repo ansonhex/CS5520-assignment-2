3b6b876d3bf1c996c6be635f889dbd1ec78d3708
ansonhex <ansonhe1997@gmail.com>
2024-10-04 15:35:05 -0700

    update type for better visualization

--- Screens/Activities.jsx
+++ Screens/Activities.jsx
@@ -1,15 +1,43 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect } from 'react';
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native';
+import { ActivityContext } from '../context/ActivityContext';
+import ItemsList from '../Components/ItemsList';
+import { useTheme } from '../context/ThemeContext';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 const Activities = ({ navigation }) => {
+  const { activities } = useContext(ActivityContext);
+  const { theme } = useTheme();
+
+  // headerRight button
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <TouchableOpacity onPress={() => navigation.navigate('AddActivitiesScreen')}>
+          <Text style={styles.header}>Add</Text>
+        </TouchableOpacity>
+      ),
+    });
+  }, [navigation, theme]);
+
   return (
-    <View>
-      <Text>Activities</Text>
-      <Button title="Add Activities" onPress={() => navigation.navigate("AddActivitiesScreen")} />
+    <View style={[styles.container, { backgroundColor: theme.backgroundColor }]}>
+      <ItemsList items={activities} type="activity" />
     </View>
-  )
-}
+  );
+};
 
-export default Activities
+export default Activities;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  header: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+    marginRight: 20,
+  },
+});

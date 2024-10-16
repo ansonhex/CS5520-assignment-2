f83f3ec5295fd6c20ba74c1d531df5bb51e94a5d
ansonhex <ansonhe1997@gmail.com>
2024-10-16 14:01:50 -0700

    added firebase update & delete

--- firebase/firebaseHelper.js
+++ firebase/firebaseHelper.js
@@ -8,6 +8,8 @@ import {
   getDocs,
 } from "firebase/firestore";
 
+// CRUD operations
+
 // Write data to Firestore
 export const writeToDB = async (collectionName, data) => {
   try {
@@ -15,5 +17,26 @@ export const writeToDB = async (collectionName, data) => {
     console.log("Document written with ID: ", docRef.id);
   } catch (error) {
     console.error("Error adding document: ", error);
+  }
+};
+
+// Update data to Firestore
+export const updateToDB = async (collectionName, id, updatedData) => {
+  try {
+    const docRef = doc(db, collectionName, id);
+    await updateDoc(docRef, updatedData);
+    console.log("Document updated with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error updating document: ", error);
+  }
+};
+
+// Delete data from Firestore
+export const deleteFromDB = async (collectionName, id) => {
+  try {
+    await deleteDoc(doc(db, collectionName, id));
+    console.log("Document deleted with ID: ", id);
+  } catch (error) {
+    console.error("Error deleting document: ", error);
   }
 };
